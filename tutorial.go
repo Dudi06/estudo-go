df299a6fbfad2c2d967d65acb2083825bcebb413
Dudi06 <neoxys1@gmail.com>
2024-11-21 10:05:51 -0300

    tutorial.go

--- tutorial.go
+++ tutorial.go
@@ -26,6 +26,10 @@ func (c circle) area() float64 {
 	return math.Pi * c.radius * c.radius
 }
 
+func getArea(s shape) float64 {
+	return s.area()
+}
+
 func main() {
 	c1 := circle{4.5}
 	r1 := rect{5, 7}
@@ -33,5 +37,6 @@ func main() {
 
 	for _, shape := range shapes {
 		fmt.Println(shape.area())
+		fmt.Println(getArea(shape))
 	}
 }
